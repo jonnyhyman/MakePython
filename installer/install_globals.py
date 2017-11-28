cf96208e82e1871436258d164155b52bd321fd7d
jonnyhyman <jonnyhyman@gmail.com>
2017-11-28 12:13:54 -0800

    version 1.0.2 push

--- installer/install_globals.py
+++ installer/install_globals.py
@@ -15,16 +15,15 @@
 """
     Making a change? Here's the make-change checklist!
         1. Make change as needed
-            1.1 make sure zip_nam matches zip_url and dl=1!!!!!!!!!!
-            1.2 make sure the folder zipped has same name as zip_nam
+            1.1 make sure the folder zipped has same name as zip_nam
 
         2. pyinstaller __make__.spec
         3. test locally
         4. test remotely / fresh install
 """
 
-zip_nam = 'editor-1-0-1'
-zip_url = "https://github.com/jonnyhyman/MakePython/files/1507802/editor-1-0-1.zip"
+zip_nam = 'editor-1-0-2'
+zip_url = "https://github.com/jonnyhyman/MakePython/files/1511476/editor-1-0-2.zip"
 pythonorg_root = "https://www.python.org/ftp/python/"
 delete_log_after = False
 pip_dependencies = {'PyQt5':'pyqt5'} # foldername / pypi name
